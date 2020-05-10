e16d2bf6c2c9313e6f4cdb966bbf0db02a8ee4d0
Shai <shai12310@gmail.com>
2020-05-10 19:20:13 +0300

    another one!

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(@"C:\Users\Shai\Desktop\testFile.txt", "hello #3");
+        }
+    }
+}
